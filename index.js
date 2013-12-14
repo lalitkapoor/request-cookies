b6efe97deb0d38a94d1610902374ab4d1dbeece7
Lalit Kapoor <lalitkapoor@gmail.com>
2013-12-13 19:42:25 -0600

    do not allow someone to pass in a jar to CookieJar

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-var CookieJar = require('./CookieJar');
-var Cookie = require('./Cookie');
+var Cookie = require('./lib/cookie');
+var CookieJar = require('./lib/cookie-jar');
 
-module.exports.CookieJar = CookieJar;
-
+/**
+ * HTTP Cookie
+ * @param {String|Object} data can be a cookie header string or a json object
+ */
 module.exports.Cookie = function(data) {
   return new Cookie(data);
+};
+
+/**
+ * Holds Cookies
+ */
+module.exports.CookieJar = function() {
+  return new CookieJar();
 };
