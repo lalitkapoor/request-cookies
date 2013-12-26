89ef850b3d7efd4b38aa1ce98d0a560abc79e5bd
Lalit Kapoor <lalitkapoor@gmail.com>
2013-12-26 13:09:48 -0600

    No need to wrap functions anymore

--- index.js
+++ index.js
@@ -3,15 +3,10 @@ var CookieJar = require('./lib/cookie-jar');
 
 /**
  * HTTP Cookie
- * @param {String|Object} data can be a cookie header string or a json object
  */
-module.exports.Cookie = function(data) {
-  return new Cookie(data);
-};
+module.exports.Cookie = Cookie;
 
 /**
- * Holds Cookies
+ * Holds cookies
  */
-module.exports.CookieJar = function() {
-  return new CookieJar();
-};
+module.exports.CookieJar = CookieJar;
