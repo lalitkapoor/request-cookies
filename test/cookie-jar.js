c5ae63487692446a0bfac088d427cfed12808711
Lalit Kapoor <lalitkapoor@gmail.com>
2014-01-18 17:19:34 -0600

    updated to use new sync api

--- test/cookie-jar.js
+++ test/cookie-jar.js
@@ -10,25 +10,25 @@ describe('CookieJar', function() {
       var jar = new cookies.CookieJar();
       var c = new cookies.Cookie('foo=bar');
       jar.add(c, url);
-      jar.getCookieHeaderString(url, function(error, header){
-         assert(header === 'foo=bar');
-      });
+
+      var header = jar.getCookieHeaderString(url)
+      assert(header === 'foo=bar');
     });
 
     it("using a cookie string", function(){
       var jar = new cookies.CookieJar();
       jar.add('foo=bar', url);
-      jar.getCookieHeaderString(url, function(error, header){
-         assert(header === 'foo=bar');
-      });
+
+      var header = jar.getCookieHeaderString(url)
+      assert(header === 'foo=bar');
     });
 
     it("using a json object", function(){
       var jar = new cookies.CookieJar();
       jar.add({key:'foo', value:'bar'}, url);
-      jar.getCookieHeaderString(url, function(error, header){
-         assert(header === 'foo=bar');
-      });
+
+      var header = jar.getCookieHeaderString(url);
+      assert(header === 'foo=bar');
     });
   });
 
@@ -36,13 +36,13 @@ describe('CookieJar', function() {
     it('should not exist in jar', function(){
       var jar = new cookies.CookieJar();
       jar.add('foo=bar', url);
-      jar.getCookieHeaderString(url, function(error, header){
-        assert(header === 'foo=bar');
-        jar.remove(url);
-        jar.getCookieHeaderString(url, function(error, header){
-          assert(header === '');
-        })
-      });
+
+      var header = jar.getCookieHeaderString(url);
+      assert(header === 'foo=bar');
+      jar.remove(url);
+
+      var header2 = jar.getCookieHeaderString(url);
+      assert(header2 === '');
     });
   });
 
@@ -50,10 +50,10 @@ describe('CookieJar', function() {
     it('should find cookies that match the domain', function(){
       var jar = new cookies.CookieJar();
       jar.add('foo=bar', url);
-      jar.getCookies(url, function(error, cookies){
-        assert(cookies.length === 1);
-        assert(cookies[0].getCookieHeaderString() === 'foo=bar');
-      });
+
+      var instances = jar.getCookies(url);
+      assert(instances.length === 1);
+      assert(instances[0].getCookieHeaderString() === 'foo=bar');
     });
   });
 });
